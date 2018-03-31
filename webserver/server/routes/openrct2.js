0fa32ea01e7ec83eb7022cc2c5ed99541670a3bd
Benjamin Zhao <yl4zhao@uwaterloo.ca>
2018-03-31 05:14:14 -0400

    adding post-able server

--- webserver/server/routes/openrct2.js
+++ webserver/server/routes/openrct2.js
@@ -9,74 +9,57 @@ var exec = require('child_process').exec;
 var router = express.Router();
 var rctConfig = require("../config/openrct2-config.json");
 
+//Multer Storage
+var storage = multer.diskStorage({
+	destination: function (req, file, callback){
+		callback(null, rctConfig.save_dir);
+	},
+	filename: function(req, file, callback){
+		var name = Date.now();
+		callback(null, name + path.extname(file.originalname));
+	}
+});
+
 //Set filters to on multer to protect server
 var rctSave = multer({
+	storage: storage,
 	fileFilter: function(req, file, callback){
-		var ext = path.extname(file.originialname);
+		var ext = path.extname(file.originalname);
 		if(ext !== '.sv6'){
-			retrun callback(new Error('Not a RCT2 save file!'));
+			return callback(new Error('Not a RCT2 save file!'));
 		}else{
 			callback(null, true);
 		}
-	}
+	},
 });
 
 //Submit a request to run an OpenRCT2 game instance
 //TODO: Perhaps guard this by authentication
 router.post('/start', rctSave.single('save'), function(req, res, next){
 	var save = req.file;
-	var port = Number.isInteger(req.body.port) ? req.body.port : rctConfig.port;
+	var port = Number.isInteger(Number(req.body.port)) ? req.body.port : rctConfig.port;
 
-	async.waterfall([
-			function(callback){
-				storeSaveFile(save, callback);
-			},
-			function(name, callback){
-				execStart(name, port, callback);
-			}
-		], function(err, result){
-			if(err){
-				res.status(500).send(err.message);
-			}
-			else{
-				console.log("Game is Starting");
-				res.send(result);
-			}
-		});
+	console.log(save);
+	execStart(save.filename, port, function(err, result){
+		if(err){
+			res.status(500).send(err.msg);
+		}else{
+			res.status(400).send("Got It!");
+		}
+	});
 });
 
 // FUNCTION CALLS //////////////////////////////////////////////////////////////
 
-function storeSaveFile(save, callback){
-	var name = Date.now();
-	var storage = multer.diskStorage({
-		destination: function (req, file, callback){
-			callback(null, rctConfig.save_dir);
-		},
-		filename: function(req, file, callback){
-			callback(null, name + path.extname(file.originalname));
-		}
-	});
-
-	var upload = multer({ storage: storage });
-	upload(req, res, function(err){
-		if(err){
-			callback(err, null);	
-		}else{
-			callback(null, name);
-		}
-	});
-}
-
 function execStart(name, port, callback){
-	var exec = "../" + rctConfig.script_dir + rctConfig.exec;
-	var save = "../" + rctConfig.save_dir + name;
+	var executable = "../" + rctConfig.script_dir + "/" + rctConfig.exec;
+	var save = rctConfig.save_dir + "/" + name;
 
-	var cmd = exec + " " + save + " " + port;
+	var cmd = executable + " " + save + " " + port;
 	console.log(cmd);
 
-	exec(cmd, function(err, stdout, stderr){
-		if(err){
+	var child = exec(cmd, function(err, stdout, stderr){
+		if(err || stderr){
 			console.log(err);
 			callback(err, null);
 		}else{
